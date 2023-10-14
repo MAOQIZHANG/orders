7805b6edabee0bf89da32309d4db9f07bd0690c7
Mike Wang <ww1284@nyu.edu>
2023-10-14 17:19:46 +0000

    model setup complete

--- service/models.py
+++ service/models.py
@@ -5,6 +5,7 @@ All of the models are stored in this module
 """
 import logging
 from datetime import datetime
+from enum import Enum
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -24,102 +25,15 @@ class DataValidationError(Exception):
     """Used for an data validation errors when deserializing"""
 
 
-class Order(db.Model):
-    """
-    Class that represents a Order
-    """
+class OrderStatus(Enum):
+    """Enumeration of valid Order Status"""
 
-    app = None
-
-    # Table Schema
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
-    create_time = db.Column(db.datetime, default=datetime.utcnow)
-    address = db.Column(db.String(255), nullable=False)
-    cost_amount = db.Column(db.Float, nullable=False)
-    status = db.Column(db.String(50), nullable=False)
-    items = db.relationship("Item", backref="order", lazy=True)
-
-    def __repr__(self):
-        return f"<Order {self.name} id=[{self.id}]>"
-
-    def create(self):
-        """
-        Creates a Order to the database
-        """
-        logger.info("Creating %s", self.name)
-        self.id = None  # pylint: disable=invalid-name
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        """
-        Updates a Order to the database
-        """
-        logger.info("Saving %s", self.name)
-        db.session.commit()
-
-    def delete(self):
-        """Removes a Order from the data store"""
-        logger.info("Deleting %s", self.name)
-        db.session.delete(self)
-        db.session.commit()
-
-    def serialize(self):
-        """Serializes a Order into a dictionary"""
-        return {"id": self.id, "name": self.name}
-
-    def deserialize(self, data):
-        """
-        Deserializes a Order from a dictionary
-
-        Args:
-            data (dict): A dictionary containing the resource data
-        """
-        try:
-            self.name = data["name"]
-        except KeyError as error:
-            raise DataValidationError(
-                "Invalid Order: missing " + error.args[0]
-            ) from error
-        except TypeError as error:
-            raise DataValidationError(
-                "Invalid Order: body of request contained bad or no data - "
-                "Error message: " + error
-            ) from error
-        return self
-
-    @classmethod
-    def init_db(cls, app):
-        """Initializes the database session"""
-        logger.info("Initializing database")
-        cls.app = app
-        # This is where we initialize SQLAlchemy from the Flask app
-        db.init_app(app)
-        app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
-
-    @classmethod
-    def all(cls):
-        """Returns all of the Orders in the database"""
-        logger.info("Processing all Orders")
-        return cls.query.all()
-
-    @classmethod
-    def find(cls, by_id):
-        """Finds a Order by it's ID"""
-        logger.info("Processing lookup for id %s ...", by_id)
-        return cls.query.get(by_id)
-
-    @classmethod
-    def find_by_name(cls, name):
-        """Returns all Orders with the given name
-
-        Args:
-            name (string): the name of the Orders you want to match
-        """
-        logger.info("Processing name query for %s ...", name)
-        return cls.query.filter(cls.name == name)
+    NEW = 0
+    PENDING = 1
+    APPROVED = 2
+    SHIPPED = 3
+    DELIVERED = 4
+    CANCELED = 5
 
 
 class Item(db.Model):
@@ -131,7 +45,9 @@ class Item(db.Model):
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=False)
+    order_id = db.Column(
+        db.Integer, db.ForeignKey("order.id", ondelete="CASCADE"), nullable=False
+    )
     title = db.Column(db.String(255), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=False)
@@ -139,14 +55,14 @@ class Item(db.Model):
     status = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
-        return f"<Item {self.name} id=[{self.id}]>"
+        return f"<Item {self.title} id=[{self.id}]>"
 
     def create(self):
         """
         Creates a Item to the database
         """
-        logger.info("Creating %s", self.name)
-        self.id = None  # pylint: disable=invalid-name
+        logger.info("Creating %s", self.title)
+        self.id = None  # pylint: disable=invalid-title
         db.session.add(self)
         db.session.commit()
 
@@ -154,18 +70,26 @@ class Item(db.Model):
         """
         Updates a Item to the database
         """
-        logger.info("Saving %s", self.name)
+        logger.info("Saving %s", self.title)
         db.session.commit()
 
     def delete(self):
         """Removes a Item from the data store"""
-        logger.info("Deleting %s", self.name)
+        logger.info("Deleting %s", self.title)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
         """Serializes a Item into a dictionary"""
-        return {"id": self.id, "name": self.name}
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "title": self.title,
+            "amount": self.amount,
+            "price": self.price,
+            "product_id": self.product_id,
+            "status": self.status,
+        }
 
     def deserialize(self, data):
         """
@@ -175,7 +99,11 @@ class Item(db.Model):
             data (dict): A dictionary containing the resource data
         """
         try:
-            self.name = data["name"]
+            self.order_id = data["order_id"]
+            self.title = data["title"]
+            self.price = data["price"]
+            self.product_id = data["product_id"]
+            self.status = data["status"]
         except KeyError as error:
             raise DataValidationError(
                 "Invalid Item: missing " + error.args[0]
@@ -183,7 +111,7 @@ class Item(db.Model):
         except TypeError as error:
             raise DataValidationError(
                 "Invalid Item: body of request contained bad or no data - "
-                "Error message: " + error
+                "Error message: " + str(error)
             ) from error
         return self
 
@@ -215,6 +143,130 @@ class Item(db.Model):
 
         Args:
             name (string): the name of the Items you want to match
+        """
+        logger.info("Processing name query for %s ...", name)
+        return cls.query.filter(cls.name == name)
+
+
+class Order(db.Model):
+    """
+    Class that represents a Order
+    """
+
+    app = None
+
+    # Table Schema
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63))
+    create_time = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    address = db.Column(db.String(255), nullable=False)
+    cost_amount = db.Column(db.Float, nullable=False)
+    status = db.Column(
+        db.Enum(OrderStatus), nullable=False, server_default=(OrderStatus.NEW.name)
+    )
+    items = db.relationship("Item", backref="order", lazy=True, passive_deletes=True)
+
+    def __repr__(self):
+        return f"<Order {self.name} id=[{self.id}]>"
+
+    def create(self):
+        """
+        Creates a Order to the database
+        """
+        logger.info("Creating %s", self.name)
+        self.id = None  # pylint: disable=invalid-name
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Updates a Order to the database
+        """
+        logger.info("Saving %s", self.name)
+        db.session.commit()
+
+    def delete(self):
+        """Removes a Order from the data store"""
+        logger.info("Deleting %s", self.name)
+        db.session.delete(self)
+        db.session.commit()
+
+    def serialize(self):
+        """Serializes a Order into a dictionary"""
+        order = {
+            "id": self.id,
+            "name": self.name,
+            "create_time": self.create_time.isoformat(),
+            "address": self.address,
+            "cost_amount": self.cost_amount,
+            "status": self.status.name,
+            "items": [],
+        }
+
+        for item in self.items:
+            order["items"].append(item.serialize())
+
+        return order
+
+    def deserialize(self, data):
+        """
+        Deserializes a Order from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.name = data["name"]
+            self.create_time = datetime.fromisoformat(data["create_time"])
+            self.address = data["address"]
+            self.cost_amount = data["cost_amount"]
+            self.status = getattr(OrderStatus, data["status"])
+            item_list = data["items"]
+
+            for json_item in item_list:
+                item = Item()
+                item.deserialize(json_item)
+                self.items.append(item)
+
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid Order: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Order: body of request contained bad or no data - "
+                "Error message: " + str(error)
+            ) from error
+        return self
+
+    @classmethod
+    def init_db(cls, app):
+        """Initializes the database session"""
+        logger.info("Initializing database")
+        cls.app = app
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
+
+    @classmethod
+    def all(cls):
+        """Returns all of the Orders in the database"""
+        logger.info("Processing all Orders")
+        return cls.query.all()
+
+    @classmethod
+    def find(cls, by_id):
+        """Finds a Order by it's ID"""
+        logger.info("Processing lookup for id %s ...", by_id)
+        return cls.query.get(by_id)
+
+    @classmethod
+    def find_by_name(cls, name):
+        """Returns all Orders with the given name
+
+        Args:
+            name (string): the name of the Orders you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
