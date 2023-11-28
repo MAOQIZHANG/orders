d998032e774282679034c9efcef83390a5ee2f3a
MAOQIZHANG <m271881653@icloud.com>
2023-11-28 14:18:25 -0500

    add dependency for bdd

--- features/steps/orders_steps.py
+++ features/steps/orders_steps.py
@@ -15,9 +15,9 @@
 ######################################################################
 
 """
-Pet Steps
+Order Steps
 
-Steps file for Pet.feature
+Steps file for Order.feature
 
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
@@ -30,21 +30,22 @@ HTTP_200_OK = 200
 HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
 
-@given('the following pets')
+@given('the following orders')
 def step_impl(context):
-    """ Delete all Pets and load new ones """
+    """ Delete all Orders and load new ones """
 
     # List all of the pets and delete them one by one
-    rest_endpoint = f"{context.base_url}/pets"
+    rest_endpoint = f"{context.base_url}/orders"
     context.resp = requests.get(rest_endpoint)
     assert(context.resp.status_code == HTTP_200_OK)
-    for pet in context.resp.json():
-        context.resp = requests.delete(f"{rest_endpoint}/{pet['id']}")
+    for order in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{order['id']}")
         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
 
     # load the database with new pets
     for row in context.table:
         payload = {
+            "id": row['id'],
             "name": row['name'],
             "category": row['category'],
             "available": row['available'] in ['True', 'true', '1'],
